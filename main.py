6a7ebba049a22400c3bb418f501f02229f2e360a
heguichuan <heguichuan@cmiot.chinamobile.com>
2021-05-08 18:48:19 +0800

    test

--- main.py
+++ main.py
@@ -17,12 +17,15 @@ import os
 import shutil
 import hashlib
 from functools import partial
-from multiprocessing import Pool, Manager
+# from multiprocessing import Pool, Manager
 import glob
 from hachoir import parser,metadata
 from geopy.geocoders import Nominatim
 from configparser import ConfigParser
 import ffmpeg # pip install ffmpeg-python
+import subprocess
+import json
+import time
 
 # 自定义程序运行参数
 mime_types = ['jpeg', 'png', 'jpg', 'webp', 'gif', 'bmp', 'mp4', 'mov', 'm4v', 'wmv', 'avi', 'rm', 'rmvb']
@@ -31,7 +34,7 @@ dist_root = os.path.join(root, 'dist')
 no_create_time_root = os.path.join(root, '无拍摄时间')
 log_file = os.path.join(root, '重复文件记录.txt')
 config_path = os.path.join(root, 'config.ini')
-exiftool_bin=
+exiftool_bin = '/usr/local/bin/exiftool'
 ###################################################################
 
 geolocator = Nominatim(user_agent="imagepy")
@@ -74,12 +77,9 @@ def ensure_path():
     if not os.path.exists(dist_root):
         os.mkdir(dist_root)
 
-def get_images_path(media_type, images, lock):
+def get_images_path(media_type):
     pattern = os.path.join(root, '**/*.' + media_type)
-    images_part = glob.glob(pattern, recursive=True)
-    lock.acquire()
-    images.extend(images_part)
-    lock.release()
+    return glob.glob(pattern, recursive=True)
 
 def check_md5(f):
     d = hashlib.md5()
@@ -144,7 +144,7 @@ class ExifTool(object):
     # windows: sentinel = "{ready}\r\n" #todo
     sentinel = "{ready}\n"
 
-    def __init__(self, executable="/usr/local/bin/exiftool"):
+    def __init__(self, executable=exiftool_bin):
         self.executable = executable
 
     def __enter__(self):
@@ -170,6 +170,23 @@ class ExifTool(object):
 
     def get_metadata(self, *filenames):
         return json.loads(self.execute("-G", "-j", "-n", *filenames))
+
+def travel_files(path, record_files, duplicated_files, lock):
+    with open(path, 'rb') as f:
+        md5 = check_md5(f)
+    unique_key = (os.path.getsize(path), md5.hexdigest())
+
+    lock.acquire()
+    if unique_key in record_files:
+        if unique_key in duplicated_files:
+            duplicated_files[unique_key] = duplicated_files[unique_key] + ',' + path
+        else:
+            duplicated_files[unique_key] = record_files[unique_key] + ',' + path
+        lock.release()
+        return
+    record_files[unique_key] = '**-' + unique_key[1][8:24] + os.path.splitext(path)[1]
+    lock.release()
+    # todo 不查md5，使用创建时间\文件大小\gps\来决定唯一性。对于没有创建时间的再计算md5
 
 def move_file(path, record_files, duplicated_files, lock):
     with open(path, 'rb') as f:
@@ -220,26 +237,37 @@ if __name__=="__main__":
     print('开始处理...')
     cpu_count = os.cpu_count()
     images = []
-    with Manager() as manager1:
-        images_proxy = manager1.list()
-        lock1 = manager1.Lock()
-        pl1 = Pool(cpu_count - 1 if cpu_count - 1 > 1 else 1)
-        for t in mime_types:
-            pl1.apply_async(get_images_path, args=(t, images_proxy, lock1))
-        pl1.close()
-        pl1.join()
-        images.extend(images_proxy)
+    for t in mime_types:
+        images.extend(get_images_path(t))
 
-    with Manager() as manager2:
-        record_files = manager2.dict() #保存已记录的文件信息
-        duplicated_files = manager2.dict() #保存重复的文件信息
-        lock2 = manager2.Lock()
+    # with Manager() as manager2:
+    #     record_files = manager2.dict() #保存已记录的文件信息
+    #     duplicated_files = manager2.dict() #保存重复的文件信息
+    #     lock2 = manager2.Lock()
 
-        pl2 = Pool(cpu_count - 1 if cpu_count - 1 > 1 else 1)
-        for image_path in images:
-            pl2.apply_async(move_file, args=(image_path, record_files, duplicated_files, lock2))
-        pl2.close()
-        pl2.join()
-        output_duplicated_files(duplicated_files)
+    #     pl2 = Pool(cpu_count - 1 if cpu_count - 1 > 1 else 1)
+    #     for image_path in images:
+    #         pl2.apply_async(move_file, args=(image_path, record_files, duplicated_files, lock2))
+    #     pl2.close()
+    #     pl2.join()
+    #     output_duplicated_files(duplicated_files)
+    
+
+    with ExifTool() as ex:
+        pointer = 0
+        skip = 1
+        while pointer < len(images):
+            with open(images[pointer], 'rb') as f:
+                print(time.time())
+                print(images[pointer])
+                start = time.time()
+                md5 = check_md5(f)
+                md5.hexdigest()
+                print(time.time() - start)
+            pointer += skip
+            metas = ex.get_metadata(*images[pointer-skip:pointer])
+            
+            # print(metas)
+            print('-----------')
 
     print('处理完成啦!')
